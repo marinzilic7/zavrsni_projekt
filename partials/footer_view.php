a18fa60eff5232c7136bd87c8abee43411ce33a1
Contineo <marinzilic507@gmail.com>
2022-10-12 18:58:29 +0200

    update

---
+++ partials/footer_view.php
@@ -0,0 +1,44 @@
+<div class="footer">
+            <div class="footer_1">
+                <ul>
+                    <li>Customer Service</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Savings Comparision</li>
+                    <li>Become a Partner</li>
+                    <li>Become a Reseller</li>
+                    <li>Terms and Conditions</li>
+                    <li>Terms of Use</li>
+                    <li>Privacy Policy</li>
+                    <li>Our Brand</li>
+                    <li>Unsubscribe</li>
+                </ul>
+            </div>
+            <div class="footer_1">
+                <ul>
+                    <li>Top Cuisines</li>
+                    <li>Indian</li>
+                    <li>Chinese</li>
+                    <li>Italian</li>
+                    <li>Pizza</li>
+                    <li>Japanese</li>
+                </ul>
+            </div>
+            <div class="footer_1">
+                <ul>
+                    <li>Locations</li>
+                    <li>Carlisle</li>
+                    <li>Stoke On Trent</li>
+                    <li>Edinburgh</li>
+                    <li>Cardiff</li>
+                </ul>
+            </div>
+            <div class="credit_card">
+                <img src="https://foodhub.co.uk/compressed_images/verifiedby.svg" alt="" height="30px" width="153px">
+            </div>
+        </div>
+    </div>
+
+    <script src="./app/app.js"></script>
+</body>
+</html> 
