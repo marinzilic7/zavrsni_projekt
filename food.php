857a0e4617c7cbe71f5651507491d2575b375809
Contineo <marinzilic507@gmail.com>
2022-10-13 00:47:14 +0200

    first commit

--- food.php
+++ food.php
@@ -2,9 +2,13 @@
 
 require 'connection.php'; 
 
-$sql = 'SELECT * FROM food';
+$sql = 'SELECT * FROM soup';
 $query = mysqli_query($db,$sql); 
-$result = mysqli_fetch_all($query, MYSQLI_ASSOC); 
+$result = mysqli_fetch_all($query, MYSQLI_ASSOC);
+
+$sql_2 = 'SELECT * FROM burgers';
+$query_2 = mysqli_query($db,$sql_2); 
+$result_2 = mysqli_fetch_all($query_2, MYSQLI_ASSOC);
 
 
 
--- food_view.php
+++ food_view.php
@@ -28,3 +28,26 @@
     <?php endforeach; ?>
 </div>
 
+<div class="burger_head">
+    <h1>Burgers</h1>
+</div>
+
+<div class="burger">
+    <?php foreach($result_2 as $value_2): ?>
+    <div class="burger_items">
+        <div class="burger_card">
+            <div class="burger_img">
+                <img src="./styles/images<?php echo $value_2['image']?>" alt="" height="100px" width="150px" >
+            </div>
+            <div class="burger_info">
+                <p><?php echo $value_2['name'] ?></p>
+                <p><?php echo $value_2['price'] ?></p>
+            </div>
+            <div class="adonii">
+                <button>Add</button>
+            </div>
+        </div>
+    </div>
+    <?php endforeach; ?>
+</div>
+
