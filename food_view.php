653db88a4f0e9f14bf1f86a3393b64ab8130a357
Contineo <marinzilic507@gmail.com>
2022-10-12 20:33:25 +0200

    update

--- food_view.php
+++ food_view.php
@@ -24,14 +24,24 @@ require 'food.php';
     </div>
 
     <div class="soup_card">
+        
         <?php foreach($result as $value): ?>
-        <div class="soup_img">
-            <img src="./styles/images<?php echo $value['image'] ?>" alt="" height="150px" width="150px">
-        </div>
-        <div class="soup_info">
-            <p><?php echo $value['name'] ?></p>
-            <p><?php echo $value['price'] ?></p>
-        </div>
-        <?php endforeach; ?>
+        <div class="soup_poredak">
+            <div class="soup_items">
+                    <div class="soup_img">
+                        <img src="./styles/images<?php echo $value['image'] ?>" alt="" height="150px" width="150px">
+                    </div>
+                <div class="soup_info">
+                    <p><?php echo $value['name'] ?></p>
+                    <p><?php echo $value['price'] ?></p>
+                </div>
+                <div class="adoni">
+                    <button>Add</button>
+                </div>
+            </div>
+         <?php endforeach; ?>
+                
+        </div> 
+             
     </div>
 </div>
